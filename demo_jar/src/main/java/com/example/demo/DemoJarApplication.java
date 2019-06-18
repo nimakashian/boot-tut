0eae7423bd0ce639adc21f75b7ab493c4ffa4322
morteza kashian <a@a.com>
2019-06-18 17:21:40 +0430

    boot tut

--- demo_jar/src/main/java/com/example/demo/DemoJarApplication.java
+++ demo_jar/src/main/java/com/example/demo/DemoJarApplication.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,10 +18,13 @@ public class DemoJarApplication {
 }
 @Component
 class MyComp{
+    @Value("${data.server}")
+    private String server;
 
     @Autowired
     public MyComp(ApplicationArguments args){
         System.out.println("heloooooooooooooo boot");
+        System.out.println(server);
         if(args.containsOption("aaa"))
             System.out.println("contains AAAAAAAAAAAAAAAAAAAAAAAAAA");
         if(!args.getNonOptionArgs().isEmpty())
