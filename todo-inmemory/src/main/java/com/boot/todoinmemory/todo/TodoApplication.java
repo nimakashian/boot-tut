de3d1624235bf683a22c407fe5153cd8bd00e5f8
morteza kashian <a@a.com>
2019-06-22 12:00:11 +0430

    check the git gui

---
+++ todo-inmemory/src/main/java/com/boot/todoinmemory/todo/TodoApplication.java
@@ -0,0 +1,16 @@
+package com.boot.todoinmemory.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoApplication.class, args);
+
+        //TODO
+        //some changes to check the git ui
+    }
+
+}
