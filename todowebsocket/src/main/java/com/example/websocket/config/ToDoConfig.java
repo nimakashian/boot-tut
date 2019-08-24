ae83850eb1b40504f25d37da8d745b3d17221ae7
morteza kashian <a@a.com>
2019-08-24 14:49:20 +0430

    todo websocket

---
+++ todowebsocket/src/main/java/com/example/websocket/config/ToDoConfig.java
@@ -0,0 +1,35 @@
+package com.example.websocket.config;
+
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+@EnableConfigurationProperties(ToDoProperties.class)
+public class ToDoConfig implements WebSocketMessageBrokerConfigurer {
+
+    private ToDoProperties props;
+
+    public ToDoConfig(ToDoProperties props) {
+        this.props = props;
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry
+                .addEndpoint(props.getEndpoint())
+                .setAllowedOrigins("*")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker(props.getBroker());
+        registry.setApplicationDestinationPrefixes(props.getApp());
+    }
+}
---
+++ todowebsocket/src/main/java/com/example/websocket/event/ToDoEventHandler.java
@@ -0,0 +1,32 @@
+package com.example.websocket.event;
+
+
+import com.example.websocket.config.ToDoProperties;
+import com.example.websocket.domain.ToDo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.rest.core.annotation.HandleAfterCreate;
+import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RepositoryEventHandler(ToDo.class)
+public class ToDoEventHandler {
+
+    private Logger log = LoggerFactory.getLogger(ToDoEventHandler.class);
+    private SimpMessagingTemplate simpMessagingTemplate;
+    private ToDoProperties toDoProperties;
+
+    public ToDoEventHandler(SimpMessagingTemplate simpMessagingTemplate,
+                            ToDoProperties toDoProperties) {
+        this.simpMessagingTemplate = simpMessagingTemplate;
+        this.toDoProperties = toDoProperties;
+    }
+
+    @HandleAfterCreate
+    public void handleToDoSave(ToDo toDo){
+        this.simpMessagingTemplate.convertAndSend(this.toDoProperties.getBroker()+"/new",toDo);
+        log.info(">> Sending Message to WS: ws://todo/new - " + toDo);
+    }
+}
